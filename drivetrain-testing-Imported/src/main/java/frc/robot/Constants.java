7d11570b5cc122fc2d19c6ed58e43b252e16c315
Gitter499 <rafayel.amirkhanyan@gmail.com>
2022-10-24 16:08:15 -0400

    UNTESTED: Finished basic drivetrain

---
+++ drivetrain-testing-Imported/src/main/java/frc/robot/Constants.java
@@ -0,0 +1,31 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.SpeedController;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+
+    // public static SpeedController K_TRACK_WIDTH;
+
+    public static final int FRONT_LEFT_MOTOR = 0; 
+    public static final int MIDDLE_LEFT_MOTOR = 1; 
+    public static final int BACK_LEFT_MOTOR = 3; 
+    public static final int FRONT_RIGHT_MOTOR = 4; 
+    public static final int MIDDLE_RIGHT_MOTOR = 5; 
+    public static final int BACK_RIGHT_MOTOR = 6; 
+
+}
+
+
+

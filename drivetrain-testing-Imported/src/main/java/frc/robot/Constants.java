08f6be64557d3c2d2569f4d340fc90368b7a7b1f
BananasAmIRite <jasony1230@gmail.com>
2022-10-28 19:43:26 +0000

    offloaded controller port to constants

--- drivetrain-testing-Imported/src/main/java/frc/robot/Constants.java
+++ drivetrain-testing-Imported/src/main/java/frc/robot/Constants.java
@@ -25,6 +25,7 @@ public final class Constants {
     public static final int MIDDLE_RIGHT_MOTOR = 4; 
     public static final int BACK_RIGHT_MOTOR = 5; 
 
+    public static final int DRIVER_CONTROLLER = 0; 
 }
 
 
